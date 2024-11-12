65a022f2bda0960dc02a9163127f4fccd7641d34
JayFromProgramming <ajsweene@mtu.edu>
2024-11-12 17:14:36 -0500

    Adding second room device, eventually this will be split into separate repo's

--- src/Devices/Radiator.cpp
+++ src/Devices/Radiator.cpp
@@ -16,6 +16,11 @@ Radiator::Radiator() {
         const auto radiator = static_cast<Radiator*>(self);
         radiator->lastHeartbeat = xTaskGetTickCount();
     });
+    addEventCallback("radiator_temp_update", [](RoomDevice* self, const ParsedEvent_t* data) {
+        const auto radiator = static_cast<Radiator*>(self);
+        radiator->updateRadiatorTemp(data->args[0].value.floatVal);
+    });
+
 }
 
 void Radiator::startTask(TaskHandle_t* taskHandle) {
@@ -23,7 +28,7 @@ void Radiator::startTask(TaskHandle_t* taskHandle) {
         "Radiator", STACK_SIZE, this, PRIORITY, taskHandle);
 }
 
-void Radiator::RTOSLoop(void* pvParameters) {
+[[noreturn]] void Radiator::RTOSLoop(void* pvParameters) {
     auto* self = static_cast<Radiator *>(pvParameters);
     TickType_t xLastWakeTime = xTaskGetTickCount();
     for (;;) {
@@ -36,15 +41,128 @@ void Radiator::RTOSLoop(void* pvParameters) {
                 self->heartbeat_expired = true;
             }
         }
+        switch(self->state) {
+            case CLOSING: // Radiator is closing the valve
+                if (self->radiator_temp < self->temp_at_shutdown - 1.5)
+                    self->state = COOLDOWN;
+                if (self->radiator_temp <= RADIATOR_COOLDOWN_TEMP)
+                    self->state = OFF;
+                if (xTaskGetTickCount() - self->cooldown_start > RADIATOR_COOLDOWN_TIME)
+                    self->state = SHUTDOWN_FAULT;
+            case COOLDOWN:
+                if (self->radiator_temp <= RADIATOR_COOLDOWN_TEMP)
+                    self->state = OFF;
+                // Check if the temperature is currently decreasing
+                if (self->radiator_temp < self->last_radiator_temp)
+                    self->cooldown_start = xTaskGetTickCount();
+                if (xTaskGetTickCount() - self->cooldown_start > RADIATOR_COOLDOWN_TIME)
+                    self->state = SHUTDOWN_FAULT;
+            break;
+            case OFF: // Radiator is off and cold (and should stay cold)
+                if (self->radiator_temp > RADIATOR_COOLDOWN_TEMP && !self->on)
+                    self->state = SHUTDOWN_FAULT;
+                if (self->on) self->state = WARMUP; // Edge case check
+                break;
+            case OPENING:
+                if (self->radiator_temp > self->temp_at_startup + 1.5)
+                    self->state = WARMUP;
+                if (self->radiator_temp >= RADIATOR_OPERATING_TEMP)
+                    self->state = ON;
+                if (xTaskGetTickCount() - self->warmup_start > RADIATOR_HEATUP_TIME)
+                    self->state = STARTUP_FAULT;
+                break;
+            case WARMUP:
+                if (self->radiator_temp >= RADIATOR_OPERATING_TEMP)
+                    self->state = ON;
+                // Check if the temperature is currently increasing
+                if (self->radiator_temp > self->last_radiator_temp)
+                    self->warmup_start = xTaskGetTickCount();
+                if (self->radiator_temp < self->temp_at_startup - 2)
+                    self->state = STARTUP_FAULT;
+                if (xTaskGetTickCount() - self->warmup_start > RADIATOR_HEATUP_TIME)
+                    self->state = STARTUP_FAULT;
+                break;
+            case ON: // Radiator is on and at operating temperature
+                if (self->radiator_temp < RADIATOR_OPERATING_TEMP - 5 && self->on)
+                    self->state = STARTUP_FAULT;
+                if (!self->on) self->state = COOLDOWN; // Edge case check
+                break;
+
+            case STARTUP_FAULT:
+                if (self->radiator_temp > RADIATOR_OPERATING_TEMP)
+                    self->state = ON;
+                if (!self->on) self->state = OFF;
+                break;
+            case SHUTDOWN_FAULT:
+                if (self->radiator_temp < RADIATOR_COOLDOWN_TEMP)
+                    self->state = OFF;
+                if (self->on) self->state = ON;
+                break;
+
+        }
         xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000));
     }
 }
 
 void Radiator::setOn(const boolean on) {
+    // Determine the new radiator state
+    switch (state) { // The state before action
+        case OFF:
+        case CLOSING:
+        case COOLDOWN:
+            if (on) {
+                state = OPENING;
+                this->temp_at_startup = radiator_temp;
+                warmup_start = xTaskGetTickCount();
+            }
+        break;
+        case ON:
+        case OPENING:
+        case WARMUP:
+            if (!on) {
+                state = CLOSING;
+                this->temp_at_shutdown = radiator_temp;
+                cooldown_start = xTaskGetTickCount();
+            }
+        break;
+        case STARTUP_FAULT:
+        case SHUTDOWN_FAULT:
+            if (on)  state = OPENING;
+            if (on)  warmup_start = xTaskGetTickCount();
+            // if (on)  state = ON;
+            if (!on) state = CLOSING;
+            if (!on) cooldown_start = xTaskGetTickCount();
+    }
     this->on = on;
     Serial.printf("Radiator has been set %s\n", on ? "on" : "off");
     digitalWrite(RADIATOR_PIN, on ? LOW : HIGH);
+
     uplinkNow(); // Force a transmission of the device data.
+}
+
+void Radiator::updateRadiatorTemp(const float temp) {
+    // This function is called by the environment sensor to update the radiator temperature.
+    // This is used to determine if the radiator should be turned on or off.
+    this->last_radiator_temp = radiator_temp;
+    if (isnan(radiator_temp)) {
+        radiator_temp = temp;
+        return;
+    }
+    if (40 < temp && temp < 120) radiator_temp = temp;
+}
+
+const char* Radiator::getStateString() {
+    switch(state) {
+        case OFF:                   return "IDLE";
+        case WARMUP:                return "WARMUP";
+        case ON:                    return "ACTIVE";
+        case COOLDOWN:              return "COOLDOWN";
+        case STARTUP_FAULT:         return "STARTUP FAULT";
+        case SHUTDOWN_FAULT:        return "SHUTDOWN FAULT";
+        case OPENING:               return "OPENING VALVE";
+        case CLOSING:               return "CLOSING VALVE";
+    }
+    return "UNKNOWN";
 }
 
 JsonVariant Radiator::getDeviceData(){
@@ -52,6 +170,8 @@ JsonVariant Radiator::getDeviceData(){
     // deviceData["name"] = getObjectName();
     deviceData["type"] = getObjectType();
     deviceData["data"]["on"] = on;
+    deviceData["data"]["radiator_temp"] = radiator_temp;
+    deviceData["data"]["state"] = getStateString();
     deviceData["health"]["online"] = true;
     // If the heartbeat is expired, set fault and reason.
     deviceData["health"]["fault"] = heartbeat_expired;
--- src/Devices/Radiator.h
+++ src/Devices/Radiator.h
@@ -9,9 +9,35 @@
 #define RADIATOR_PIN 5
 #define RadiatorTimeout 120000  // Disable the radiator after 2 minutes no server heartbeat
 
+#define RADIATOR_HEATUP_TIME (180000 * portTICK_PERIOD_MS)   // 4 minutes
+#define RADIATOR_COOLDOWN_TIME (100000 * portTICK_PERIOD_MS) // 5 minutes
+
+// Any temperature inbetween these two values is considered a transition state
+#define RADIATOR_OPERATING_TEMP 87.0  // (F) if the radiator is above this temp, it is considered on
+#define RADIATOR_COOLDOWN_TEMP  66.0  // (F) if the radiator is below this temp, it is considered off
+
+
 class Radiator : RoomDevice {
 
+    enum RadiatorState {
+        OFF,            // Radiator is off and cold
+        OPENING,        // Radiator is opening the valve
+        WARMUP,         // Radiator is warming up to operating temperature
+        ON,             // Radiator is on and at operating temperature
+        CLOSING,        // Radiator is closing the valve
+        COOLDOWN,       // Radiator is cooling down
+        STARTUP_FAULT,  // Radiator is on but failed to reach operating temperature within the time window
+        SHUTDOWN_FAULT  // Radiator has been commanded off but is still at operating temperature
+    };
+
+    RadiatorState state = COOLDOWN;
     uint32_t lastHeartbeat = 0;
+    uint32_t warmup_start = 0;
+    uint32_t cooldown_start = 0;
+    float_t  radiator_temp = NAN;
+    float_t  last_radiator_temp = NAN;
+    float_t  temp_at_startup = NAN;
+    float_t  temp_at_shutdown = NAN;
     boolean heartbeat_expired = false;
 
 public:
@@ -35,6 +61,10 @@ public:
     Radiator();
 
     void setOn(boolean on);
+
+    void updateRadiatorTemp(float temp);
+
+    const char *getStateString();
 
     void startTask(TaskHandle_t* taskHandle) override;
 
